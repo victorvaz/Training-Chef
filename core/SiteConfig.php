c413ec1ed1a45d3ce3f9ca405e1ef056ca323d66
Víctor Vaz <victor-vaz@hotmail.com>
2015-03-23 20:06:02 -0300

    Configurações de site

---
+++ core/SiteConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Esta classe é responsável por gerenciar todas as informações estáticas do site,
+ * como, por exemplo, o nome do site.
+ */
+
+/**
+ * Classe de configuração
+ * @author Victor Vaz de Oliveira <victor-vaz@hotmail.com>
+ */
+class SiteConfig
+{
+    /**
+     * Nome do site
+     * @var String
+     */
+    public static $NOME_SITE = "Training Chef";
+    
+    /**
+     * Caminho da logotipo do site
+     * @var String
+     */
+    public static $LOGOTIPO_HORIZONTAL = "view/images/logotipo_horizontal.png";
+}
