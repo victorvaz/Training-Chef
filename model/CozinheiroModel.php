646daca8a6e8df66c97c06add231b310b5af78cc
Víctor Vaz <victor-vaz@hotmail.com>
2015-03-23 22:49:46 -0300

    Função para cadastrar um cozinheiro

--- model/CozinheiroModel.php
+++ model/CozinheiroModel.php
@@ -15,6 +15,44 @@ else { require_once 'model/Model.php'; }
  */
 class CozinheiroModel implements Model
 {
+    /**
+     * Função para cadastrar um cozinheiro.
+     * @param Cozinheiro $Cozinheiro
+     */
+    public function cadastrar(Cozinheiro $Cozinheiro)
+    {
+        $DAL = new DAL();
+        
+        $sql = "INSERT INTO cozinheiro (nome,
+                                        email,
+                                        senha)
+                                VALUES ('" . $Cozinheiro->getNome() . "',
+                                        '" . $Cozinheiro->getEmail() . "',
+                                        '" . $Cozinheiro->getSenha() . "')";
+        mysql_query($sql)
+            or die ("Aconteceu um erro: Não foi possível cadastrar o cozinheiro.");
+    }
+    
+    public function atualizar(Cozinheiro $Cozinheiro)
+    {
+        
+    }
+
+    public function buscar($id)
+    {
+        
+    }
+
+    public function buscarTodos()
+    {
+        
+    }
+
+    public function deletar($id)
+    {
+        
+    }
+
 }
 
 ?>
