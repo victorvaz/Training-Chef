1a889faa0e8095f6c5b0ebfdb65b45106bfdcec8
Víctor Vaz <victor-vaz@hotmail.com>
2015-03-25 22:13:36 -0300

    SiteConfig do modulo cozinheiro

---
+++ cozinheiro/core/SiteConfig.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * Esta classe é responsável por gerenciar todas as informações estáticas do site,
+ * como, por exemplo, o nome do site.
+ */
+
+/**
+ * Classe de configuração
+ * @author Victor Vaz de Oliveira <victor-vaz@hotmail.com>
+ */
+class SiteConfig
+{
+    /**
+     * Nome do site
+     * @var String
+     */
+    public static $NOME_SITE = "Training Chef";
+    
+    /**
+     * Caminho da logotipo do site
+     * @var String
+     */
+    public static $LOGOTIPO_HORIZONTAL = "view/images/logotipo_horizontal.png";
+    
+    /**
+     * Prefixo do endereço para as páginas
+     * @var type 
+     */
+    public static $PREFIXO_ENDERECO = "/trainingchef/cozinheiro";
+    
+    /**
+     * Páginas de menu
+     * @var type 
+     */
+    public static $MENU_PAGINAS = array(
+        0 => array(
+            'href' => 'index.php',
+            'desc' => 'Home'
+        )
+    );
+}
--- cozinheiro/view/head.php
+++ cozinheiro/view/head.php
@@ -1,7 +1,8 @@
+<?php include_once 'core/SiteConfig.php'; ?>
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Training Chef</title>
+    <title><?php print SiteConfig::$NOME_SITE; ?></title>
     <link href="view/css/bootstrap.css" rel="stylesheet" />
     <link href="view/css/font-awesome.css" rel="stylesheet" />
     <link href="view/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
