ab50b5aa4dd0f6d5a189ddf29fbf2a15831c3ca0
Víctor Vaz <victor-vaz@hotmail.com>
2015-03-25 21:33:14 -0300

    View

---
+++ cozinheiro/view/cabecalho.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-default navbar-cls-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">
+            <img src="../view/images/logotipo_horizontal.png" width="100%">
+        </a>
+    </div>
+    <div style="color: white; padding: 15px 50px 5px 50px; float: right;">
+        Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-danger square-btn-adjust">Logout</a>
+    </div>
+</nav>  
---
+++ cozinheiro/view/head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Training Chef</title>
+    <link href="view/css/bootstrap.css" rel="stylesheet" />
+    <link href="view/css/font-awesome.css" rel="stylesheet" />
+    <link href="view/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href="view/css/custom.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
